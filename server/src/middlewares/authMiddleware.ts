f5de463f1e80f90cc4193bcfca2d3a3d8adec5ca
oaktec <bdeatock@gmail.com>
2023-07-27 10:45:11 +0100

    Change authMiddleware to pass error to handler

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -1,21 +1,25 @@
 import { Request, Response, NextFunction } from "express";
+import createHttpError from "http-errors";
 import { StatusCodes } from "http-status-codes";
 
 export default {
   isAuth: (req: Request, res: Response, next: NextFunction) => {
     if (!req.isAuthenticated()) {
-      return res
-        .status(StatusCodes.UNAUTHORIZED)
-        .json({ message: "Unauthorized" });
+      return next(
+        createHttpError(
+          StatusCodes.UNAUTHORIZED,
+          "You must be logged in to access this resource"
+        )
+      );
     }
 
     next();
   },
   isGuest: (req: Request, res: Response, next: NextFunction) => {
     if (req.isAuthenticated()) {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ message: "Already logged in" });
+      return next(
+        createHttpError(StatusCodes.BAD_REQUEST, "You are already logged in")
+      );
     }
 
     next();
