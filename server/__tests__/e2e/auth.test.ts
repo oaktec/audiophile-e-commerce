704b79eaedb7eb0f4af566027962ada4bf8094a6
oaktec <bdeatock@gmail.com>
2023-07-27 10:45:24 +0100

    Add tests for auth/login route

--- server/__tests__/e2e/auth.test.ts
+++ server/__tests__/e2e/auth.test.ts
@@ -6,6 +6,7 @@ import { StatusCodes } from "http-status-codes";
 
 import db from "../../src/db";
 import { clearDatabase, createTestServer } from "../utils/testHelpers";
+import { userService } from "../../src/services";
 
 let server: Server;
 
@@ -237,6 +238,158 @@ describe("auth", () => {
 
       const dbRes = await db.query("SELECT * FROM users");
       expect(dbRes.rows.length).toEqual(0);
+    });
+
+    it("shouldn't allow a logged in user to register", async () => {
+      const agent = request.agent(server);
+
+      await agent.post("/auth/register").send({
+        email: "demo@user.com",
+        password: "Password123!",
+        firstName: "Demo",
+        lastName: "User",
+        address: "123 Test St",
+      });
+
+      await agent.post("/auth/login").send({
+        email: "demo@user.com",
+        password: "Password123!",
+      });
+
+      const res = await agent.post("/auth/register").send({
+        email: "try@again.com",
+        password: "Password123!",
+        firstName: "Try",
+        lastName: "Again",
+        address: "123 Test St",
+      });
+
+      expect(res.status).toEqual(StatusCodes.BAD_REQUEST);
+      expect(res.body).toEqual({
+        message: "You are already logged in",
+        name: "BadRequestError",
+        status: 400,
+      });
+    });
+  });
+
+  describe("POST /auth/login", () => {
+    beforeEach(async () => {
+      await request(server).post("/auth/register").send({
+        email: "demo@user.com",
+        password: "Password123!",
+        firstName: "Demo",
+        lastName: "User",
+        address: "123 Test St",
+      });
+    });
+
+    it("should login a user", async () => {
+      const res = await request(server).post("/auth/login").send({
+        email: "demo@user.com",
+        password: "Password123!",
+      });
+
+      expect(res.status).toEqual(StatusCodes.OK);
+      expect(res.body).toEqual(
+        expect.objectContaining({
+          id: expect.any(Number),
+        })
+      );
+
+      expect(res.header["set-cookie"]).toBeDefined();
+    });
+
+    function expectLogInFail(res: request.Response) {
+      expect(res.status).toEqual(StatusCodes.BAD_REQUEST);
+      expect(res.body).toEqual({
+        message: "Incorrect email or password",
+        name: "BadRequestError",
+        status: StatusCodes.BAD_REQUEST,
+      });
+
+      expect(res.header["set-cookie"]).toBeUndefined();
+    }
+
+    it("should return an error if email is incorrect", async () => {
+      const res = await request(server).post("/auth/login").send({
+        email: "demooe@user.com",
+        password: "Password123!",
+      });
+
+      expectLogInFail(res);
+    });
+
+    it("should return an error if password is incorrect", async () => {
+      const res = await request(server).post("/auth/login").send({
+        email: "demo@user.com",
+        password: "Password123",
+      });
+
+      expectLogInFail(res);
+    });
+
+    it("should return an error if email is not provided", async () => {
+      const res = await request(server).post("/auth/login").send({
+        password: "Password123!",
+      });
+
+      expectLogInFail(res);
+    });
+
+    it("should return an error if password is not provided", async () => {
+      const res = await request(server).post("/auth/login").send({
+        email: "demo@user.com",
+      });
+
+      expectLogInFail(res);
+    });
+
+    it("should return an error if body is empty", async () => {
+      const res = await request(server).post("/auth/login").send({});
+
+      expectLogInFail(res);
+    });
+
+    it("should return an error if user is already logged in", async () => {
+      const agent = request.agent(server);
+
+      await agent.post("/auth/login").send({
+        email: "demo@user.com",
+        password: "Password123!",
+      });
+
+      const res = await agent.post("/auth/login").send({
+        email: "demo.user.com",
+        password: "Password123!",
+      });
+
+      expect(res.status).toEqual(StatusCodes.BAD_REQUEST);
+      expect(res.body).toEqual({
+        message: "You are already logged in",
+        name: "BadRequestError",
+        status: StatusCodes.BAD_REQUEST,
+      });
+    });
+
+    it("should return an error if an error occurs during Passport authentication", async () => {
+      jest.spyOn(userService, "getByEmail").mockImplementationOnce(() => {
+        throw new Error("Something went wrong");
+      });
+
+      const res = await request(server).post("/auth/login").send({
+        email: "fake@user.com",
+        password: "Fake123!",
+      });
+
+      expect(res.status).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+      expect(res.body).toEqual({
+        message: "Something went wrong",
+        name: "InternalServerError",
+        status: StatusCodes.INTERNAL_SERVER_ERROR,
+      });
+
+      expect(res.header["set-cookie"]).toBeUndefined();
     });
   });
 });
