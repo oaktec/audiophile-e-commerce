b2df62743818ac662929b9ce4580d8e9a72548a1
oaktec <bdeatock@gmail.com>
2023-08-14 17:22:35 +0100

    Add Links to Navbar

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 
 import Logo from "@/assets/shared/desktop/logo.svg";
+import { Link } from "../common/Link";
 import { CartIcon, HamburgerIcon } from "../icons/Icons";
 
 const Header: FC = () => {
@@ -9,6 +10,20 @@ const Header: FC = () => {
       <div className="container flex items-center justify-between border-b border-white border-opacity-10 px-6 py-8">
         <HamburgerIcon className="lg:hidden" interactive />
         <img src={Logo} alt="logo" />
+        <div className="hidden items-center space-x-9 lg:flex">
+          <Link href="#" variant="navbar">
+            Home
+          </Link>
+          <Link href="#" variant="navbar">
+            Headphones
+          </Link>
+          <Link href="#" variant="navbar">
+            Speakers
+          </Link>
+          <Link href="#" variant="navbar">
+            Earphones
+          </Link>
+        </div>
         <CartIcon interactive />
       </div>
     </header>
