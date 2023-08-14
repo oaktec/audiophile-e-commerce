b0cb41cd9276554c3a3889ed4852d98f110c6c02
oaktec <bdeatock@gmail.com>
2023-08-14 22:38:02 +0100

    Change colour of footer

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Footer: FC = () => {
   return (
-    <footer className="w-full bg-black">
+    <footer className="w-full bg-dark-background">
       <div className="container flex justify-between">
         <h1 className="text-white">audiophile</h1>
       </div>
