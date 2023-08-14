630759553032b9464c4e0b8ae02e2148038ddd01
oaktec <bdeatock@gmail.com>
2023-08-14 16:18:38 +0100

    Add basic header implementation

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,10 +1,16 @@
 import { FC } from "react";
 
+import CartIcon from "@/assets/shared/desktop/icon-cart.svg";
+import Logo from "@/assets/shared/desktop/logo.svg";
+import HamburgerIcon from "@/assets/shared/tablet/icon-hamburger.svg";
+
 const Header: FC = () => {
   return (
-    <header className="w-full bg-black">
-      <div className="container flex justify-between">
-        <h1 className="text-white">audiophile</h1>
+    <header className="w-full bg-dark-background">
+      <div className="container flex items-center justify-between border-b border-white border-opacity-10 px-6 py-8">
+        <img className="lg:hidden" src={HamburgerIcon} alt="logo" />
+        <img src={Logo} alt="logo" />
+        <img src={CartIcon} alt="cart" />
       </div>
     </header>
   );
