ee6f948dd551449e7e799daf77d46d42dc8750d5
oaktec <bdeatock@gmail.com>
2023-08-15 08:20:32 +0100

    Remove incorrect interactive prop

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -8,7 +8,7 @@ const Header: FC = () => {
     <header className="w-full bg-dark-background">
       <div className="container flex items-center justify-between border-b border-white border-opacity-10 px-6 py-8">
         <HamburgerIcon className="lg:hidden" interactive />
-        <MainLogo interactive />
+        <MainLogo />
         <div className="hidden items-center space-x-9 lg:flex">
           <Link href="/audiophile-e-commerce/" variant="navbar">
             Home
