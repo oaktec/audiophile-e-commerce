9ca161a13d2e322dfa413874f43f6b0fe4630b0d
oaktec <bdeatock@gmail.com>
2023-08-16 09:53:17 +0100

    Add href to highlighted product link

--- client/src/pages/Home/Hero.tsx
+++ client/src/pages/Home/Hero.tsx
@@ -8,7 +8,7 @@ import { HIGHLIGHTED_PRODUCT } from "@/config/config";
 const Hero = () => (
   <div className="h-[600px] w-full bg-dark-background sm:h-[729px]">
     <div className="container relative flex h-full w-full items-center justify-center overflow-hidden lg:justify-start">
-      <div className="bg-tablet-header absolute inset-0 left-1/2 w-[750px] -translate-x-1/2 transform bg-contain bg-center bg-no-repeat opacity-50 lg:left-auto lg:translate-x-12 lg:opacity-100" />
+      <div className="absolute inset-0 left-1/2 w-[750px] -translate-x-1/2 transform bg-tablet-header bg-contain bg-center bg-no-repeat opacity-50 lg:left-auto lg:translate-x-12 lg:opacity-100" />
       <div className="z-10 mx-6 flex max-w-sm flex-col items-center gap-4 sm:gap-6 lg:items-start">
         <span className="font-manrope text-sm uppercase tracking-[10px] text-white opacity-50">
           New product
@@ -19,7 +19,9 @@ const Hero = () => (
         <TypographyHeaderDescription className="text-center lg:text-left">
           {HIGHLIGHTED_PRODUCT.description}
         </TypographyHeaderDescription>
-        <Link variant="button">See product</Link>
+        <Link variant="button" href="#">
+          See product
+        </Link>
       </div>
     </div>
   </div>
