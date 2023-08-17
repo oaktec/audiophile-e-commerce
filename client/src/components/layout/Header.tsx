6d1cec59bc3bc1a84a83e7d88470258168fb459a
oaktec <bdeatock@gmail.com>
2023-08-17 11:33:55 +0100

    Fix header container border width

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -7,32 +7,34 @@ import { CartIcon, HamburgerIcon, MainLogo, UserIcon } from "../icons/Icons";
 const Header: FC = () => {
   return (
     <header className="fixed z-50 w-full bg-dark-background">
-      <div className="container flex items-center justify-between border-b border-white border-opacity-10 px-6 py-8 sm:px-10">
-        <HamburgerIcon className="lg:hidden" interactive />
-        <Link href="/audiophile-e-commerce/">
-          <MainLogo />
-        </Link>
-        <div className="hidden items-center space-x-9 lg:flex">
-          <Link href="/audiophile-e-commerce/" variant="navbar">
-            Home
+      <div className="container">
+        <div className="mx-6 flex items-center justify-between border-b border-white border-opacity-10 py-8 sm:mx-10">
+          <HamburgerIcon className="lg:hidden" interactive />
+          <Link href="/audiophile-e-commerce/">
+            <MainLogo />
           </Link>
-          {CATEGORIES.map((category) => (
-            <Link
-              href={`/audiophile-e-commerce/category/${category}`}
-              variant="navbar"
-              key={category}
-            >
-              {category}
+          <div className="hidden items-center space-x-9 lg:flex">
+            <Link href="/audiophile-e-commerce/" variant="navbar">
+              Home
             </Link>
-          ))}
-        </div>
-        <div className="flex space-x-9">
-          <Link href="/audiophile-e-commerce/login">
-            <UserIcon interactive />
-          </Link>
-          <Link href="/audiophile-e-commerce/cart">
-            <CartIcon interactive />
-          </Link>
+            {CATEGORIES.map((category) => (
+              <Link
+                href={`/audiophile-e-commerce/category/${category}`}
+                variant="navbar"
+                key={category}
+              >
+                {category}
+              </Link>
+            ))}
+          </div>
+          <div className="flex space-x-9">
+            <Link href="/audiophile-e-commerce/login">
+              <UserIcon interactive />
+            </Link>
+            <Link href="/audiophile-e-commerce/cart">
+              <CartIcon interactive />
+            </Link>
+          </div>
         </div>
       </div>
     </header>
