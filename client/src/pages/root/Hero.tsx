bbd409423e6da8b9d03422cfaf078224e61e2bfc
oaktec <bdeatock@gmail.com>
2023-08-17 07:56:59 +0100

    Use css to soften the edge of the hero image

--- client/src/pages/root/Hero.tsx
+++ client/src/pages/root/Hero.tsx
@@ -8,7 +8,7 @@ import { HIGHLIGHTED_PRODUCTS } from "@/config/config";
 const Hero = () => (
   <div className="h-[600px] w-full bg-dark-background sm:h-[729px]">
     <div className="container relative flex h-full w-full items-center justify-center overflow-hidden lg:justify-start">
-      <div className="absolute inset-0 left-1/2 w-[750px] -translate-x-1/2 bg-tablet-header bg-contain bg-center bg-no-repeat opacity-50 lg:left-auto lg:translate-x-12 lg:opacity-100" />
+      <div className="soften-edges-to-dark-background absolute inset-0 left-1/2 w-[750px] -translate-x-1/2 bg-tablet-header bg-contain bg-center bg-no-repeat opacity-50 lg:left-auto lg:translate-x-12 lg:opacity-100" />
       <div className="z-10 mx-6 flex max-w-sm flex-col items-center gap-4 sm:gap-6 lg:items-start">
         <span className="font-manrope text-sm uppercase tracking-[10px] text-white opacity-50">
           New product
