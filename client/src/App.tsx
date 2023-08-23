58424b303ff03fb28eca54f49472f461d2085e6d
oaktec <bdeatock@gmail.com>
2023-08-23 08:33:35 +0100

    Implement UserContext

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,35 @@
+import { useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import api from "./api/api";
+import { User } from "./contexts/UserContext";
+import { useUser } from "./hooks/useUser";
 import Router from "./routes";
 
+type UserData = {
+  user: User;
+};
+
 function App() {
-  return <Router />;
+  const { setUser } = useUser();
+
+  // Check if user is logged in
+  useEffect(() => {
+    api
+      .fetch("/auth/check-session")
+      .then((data) => {
+        const { user } = data as UserData;
+        setUser(user);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, [setUser]);
+
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/contexts/UserContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+
+export type User = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  address: string;
+  id: string;
+};
+
+interface UserContextInterface {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+  isLoggedIn: boolean;
+}
+
+export const UserContext = createContext<UserContextInterface>({
+  user: null,
+  setUser: () => {},
+  isLoggedIn: false,
+});
+
+interface UserProviderProps {
+  children: React.ReactNode;
+}
+
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const isLoggedIn = !!user;
+
+  return (
+    <UserContext.Provider value={{ user, setUser, isLoggedIn }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
