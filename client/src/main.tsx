6b6941ce1275a9be1f8ebb9f96ba8df0bd764a35
oaktec <bdeatock@gmail.com>
2023-08-25 09:43:19 +0100

    Set up react-query provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App.tsx";
 import { UserProvider } from "./contexts/UserContext.tsx";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
