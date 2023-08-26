c944c8fb9ab0dbe7c48e40912751ad9b8d6ac7d7
oaktec <bdeatock@gmail.com>
2023-08-26 09:31:59 +0100

    Add picture gallery to product page

--- client/src/pages/product/ProductPage.tsx
+++ client/src/pages/product/ProductPage.tsx
@@ -41,7 +41,7 @@ const ProductPage: React.FC = () => {
             </TypographySubHeader>
           </div>
         ) : (
-          <div className="space-y-[5.5rem]">
+          <div className="flex flex-col space-y-[5.5rem]">
             <div className="flex flex-col gap-y-8 sm:flex-row sm:items-center sm:justify-between sm:gap-1">
               <picture>
                 <source
@@ -152,6 +152,55 @@ const ProductPage: React.FC = () => {
                 </div>
               </div>
               <div className="hidden lg:block" />
+            </div>
+            <div className="flex flex-col gap-5 self-center sm:flex-row">
+              <div className="flex flex-col gap-5">
+                <picture>
+                  <source
+                    media="(min-width: 1024px)"
+                    srcSet={`/product-${productSlug}/desktop/image-gallery-1.jpg`}
+                  />
+                  <source
+                    media="(min-width: 640px)"
+                    srcSet={`/product-${productSlug}/tablet/image-gallery-1.jpg`}
+                  />
+                  <img
+                    src={`/product-${productSlug}/mobile/image-gallery-1.jpg`}
+                    alt={data?.name}
+                    className="rounded-lg"
+                  />
+                </picture>
+                <picture>
+                  <source
+                    media="(min-width: 1024px)"
+                    srcSet={`/product-${productSlug}/desktop/image-gallery-2.jpg`}
+                  />
+                  <source
+                    media="(min-width: 640px)"
+                    srcSet={`/product-${productSlug}/tablet/image-gallery-2.jpg`}
+                  />
+                  <img
+                    src={`/product-${productSlug}/mobile/image-gallery-2.jpg`}
+                    alt={data?.name}
+                    className="rounded-lg"
+                  />
+                </picture>
+              </div>
+              <picture>
+                <source
+                  media="(min-width: 1024px)"
+                  srcSet={`/product-${productSlug}/desktop/image-gallery-3.jpg`}
+                />
+                <source
+                  media="(min-width: 640px)"
+                  srcSet={`/product-${productSlug}/tablet/image-gallery-3.jpg`}
+                />
+                <img
+                  src={`/product-${productSlug}/mobile/image-gallery-3.jpg`}
+                  alt={data?.name}
+                  className="rounded-lg"
+                />
+              </picture>
             </div>
             <div />
           </div>
