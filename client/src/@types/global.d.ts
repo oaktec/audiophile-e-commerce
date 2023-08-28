b6c9431c855c7e7d9ad973e2781d228b67f9b2b4
oaktec <bdeatock@gmail.com>
2023-08-28 11:34:47 +0100

    Add Cart and CartItem type

--- client/src/@types/global.d.ts
+++ client/src/@types/global.d.ts
@@ -13,3 +13,17 @@ interface FullProduct extends Product {
   similarProducts: string[];
   boxContents: { item: string; quantity: number }[];
 }
+
+type CartItem = {
+  slug: string;
+  quantity: number;
+  name: string;
+  price: number;
+};
+
+type Cart = {
+  id: number;
+  userId: number;
+  active: boolean;
+  items: CartItem[];
+};
