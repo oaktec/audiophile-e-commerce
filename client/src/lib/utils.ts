2330dc0399881bb0dbf3b0dd4c68f808c21c68f1
oaktec <bdeatock@gmail.com>
2023-08-28 11:39:03 +0100

    Add shortenName fn

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function shortenName(name: string) {
+  const words = name.split(" ");
+  words.pop();
+  return words.join(" ");
 }
