8650ea1dba677bf174a0141c9e29c641ed1c7e30
oaktec <bdeatock@gmail.com>
2023-08-31 16:04:56 +0100

    Add stripe-secret route to backend

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -36,6 +36,7 @@ export const CORS_CLIENT_URL = ensureEnvVar(
   NODE_ENV === "production",
   "http://localhost:5173"
 );
+export const STRIPE_SECRET_KEY = ensureEnvVar("STRIPE_SECRET_KEY");
 
 const DB_HOST = ensureEnvVar("DB_HOST", NODE_ENV !== "production");
 const DB_NAME = ensureEnvVar("DB_NAME", NODE_ENV !== "production");
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -5,6 +5,7 @@ import cartRouter from "./cart";
 import productsRouter from "./products";
 import ordersRouter from "./orders";
 import usersRouter from "./users";
+import { paymentIntent } from "../config/stripe";
 
 const router = Router();
 
@@ -16,6 +17,18 @@ router.use("/users", usersRouter);
 
 router.get("/health", (req, res) => {
   res.sendStatus(200);
+});
+
+router.get("/stripe-secret", async (req, res) => {
+  const amount = req.query.amount;
+  if (!amount || isNaN(Number(amount))) {
+    res.status(400).json({ error: "Invalid amount" });
+    return;
+  }
+
+  const intent = await paymentIntent(Number(amount));
+
+  res.json({ clientSecret: intent.client_secret });
 });
 
 export default router;
