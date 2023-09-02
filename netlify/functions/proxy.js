fb7c89f0b83f48257cbee3eab259d7ea89e003fb
oaktec <bdeatock@gmail.com>
2023-09-02 17:39:14 +0100

    Attempt fix to proxy function

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,24 +1,30 @@
 const axios = require("axios");
 
 exports.handler = async (event, context) => {
-  const { httpMethod, headers, queryStringParameters, body } = event;
+  const { httpMethod, path, headers, queryStringParameters, body } = event;
 
   try {
+    const url = new URL(
+      decodeURIComponent(event.path.split(".netlify/functions/proxy/")[1])
+    );
+
+    for (const i in queryStringParameters) {
+      url.searchParams.append(i, queryStringParameters[i]);
+    }
+
     const response = await axios({
       method: httpMethod,
-      url: `https://audiophile-e-commerce-server.fly.dev${event.path}`,
+      url,
       headers: {
         ...headers,
-        "Access-Control-Allow-Origin":
-          "https://audiophile-e-commerce-beneatock.netlify.app",
-        "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-        "Access-Control-Allow-Headers": "Content-Type",
+        "content-type": "application/json",
+        accept: "*/*",
+        host: url.host,
       },
-      params: queryStringParameters,
-      data: body,
+      body,
       withCredentials: true,
     });
+
     return {
       statusCode: response.status,
       headers: {
