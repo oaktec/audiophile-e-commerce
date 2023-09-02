1b066db689716ae11974cbee12c1b498758f19fd
oaktec <bdeatock@gmail.com>
2023-09-02 07:59:25 +0100

    Fix attribute names on confirmation icon

--- client/src/components/icons/Icons.tsx
+++ client/src/components/icons/Icons.tsx
@@ -28,11 +28,11 @@ export const MainLogo: React.FC = () => (
 
 export const OrderConfirmation: React.FC = () => (
   <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
-    <g fill="none" fill-rule="evenodd">
+    <g fill="none" fillRule="evenodd">
       <circle fill="#D87D4A" cx="32" cy="32" r="32" />
       <path
         stroke="#FFF"
-        stroke-width="4"
+        strokeWidth="4"
         d="m20.754 33.333 6.751 6.751 15.804-15.803"
       />
     </g>
